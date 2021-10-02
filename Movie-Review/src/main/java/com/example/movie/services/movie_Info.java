38b4a9e3f90afe4a26c39bf82a9208a683fec923
Mhshohag2256 <59985721+Mhshohag2256@users.noreply.github.com>
2021-10-01 23:56:09 -0400

    Created a movie info class

--- Movie-Review/src/main/java/com/example/movie/services/movie_Info.java
+++ Movie-Review/src/main/java/com/example/movie/services/movie_Info.java
@@ -1,4 +1,28 @@
 package com.example.movie.services;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class movie_Info {
+    private String name;
+    private String description;
+    private int rating;
+
+    public movie_Info(String name, String description, int rating) {
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
