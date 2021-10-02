7791242bf55c482543b0f8f8413c2e16ac4ae03b
Mhshohag2256 <59985721+Mhshohag2256@users.noreply.github.com>
2021-10-02 00:01:38 -0400

    Created a Rest Controller class

---
+++ Movie-Review/src/main/java/com/example/movie/services/movieCatalogResource.java
@@ -0,0 +1,18 @@
+package com.example.movie.services;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class movieCatalogResource {
+
+    public List<movie_Info> getCatalog(String userId){
+        return Collections.singletonList(
+                new movie_Info("Full Metal Jacket","Civil war movie",8.5)
+        );
+    }
+
+}
--- Movie-Review/src/main/java/com/example/movie/services/movie_Info.java
+++ Movie-Review/src/main/java/com/example/movie/services/movie_Info.java
@@ -2,7 +2,6 @@ package com.example.movie.services;
 
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class movie_Info {
     private String name;
     private String description;
